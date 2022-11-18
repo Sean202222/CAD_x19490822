3116a7444313124fc37fab31ed2dc9803a930488
Sean202222 <seanjohn.mc20@gmail.com>
2022-11-18 11:43:44 +0000

    message

--- app/controllers/static_page_controller.rb
+++ app/controllers/static_page_controller.rb
@@ -1,4 +1,7 @@
 class StaticPageController < ApplicationController
+  #rails g controller StaticPage home about contact
+  #3 methods made and 3 views made
+  #routes.rb altered
   def home
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :books
-  get 'static_page/home'
-  get 'static_page/about'
-  get 'static_page/contact'
+
+  # get 'static_page/home' - made by rails g controller StaticPage... command, changed to root
+  root to: 'static_page#home'
+
+  #get 'static_page/about' - change to make pretty urls
+  get '/about', to: 'static_page#about'
+
+  #get 'static_page/contact'
+  get '/contact', to: 'static_page#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+ 
   # Defines the root path route ("/")
   # root "articles#index"
 end
